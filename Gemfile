b0a2d5c2ae2a7a57b1cad0a7e451e9321475b4d7
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2016-10-22 06:29:12 -0400

    Add messages with carrierwave, S3, and figare

--- Gemfile
+++ Gemfile
@@ -22,3 +22,11 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'rails_12factor', group: :production
 gem 'pg', group: :production
+gem 'carrierwave'
+gem "fog", "~>1.38"
+gem 'figaro'
+
+
+
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021220035) do
+ActiveRecord::Schema.define(version: 20161022075456) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20161021220035) do
     t.string   "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
