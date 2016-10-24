bb1038bdfdac5d7e4461c15e74860159b86cae83
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2016-10-24 14:29:16 -0400

    more db issues resolved

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider:              'AWS',                        # required
+    provider:              'AWS',
     aws_access_key_id:     ENV["aws_access_key_id"],   
     aws_secret_access_key: ENV["aws_secret_access_key"],
   }
