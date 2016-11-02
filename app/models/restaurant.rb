f4cf51be1ab6d0714d349e81806823d478976be8
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2016-11-02 19:01:29 -0400

    Added data validations and placeholders

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,19 @@
 class Restaurant < ApplicationRecord
   mount_uploader :image, ImageUploader
 
-  has_many :reviews
+    has_many :reviews
+
+    validates :name, :address, :phone, :website, :image, presence: true
+    validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+        message: "Must be in the format (123) 456-7890" }
+    validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+        message: "Must be in the format http(s)://" }
+    validates :address, format: { with: /\A\d+[^,]+, [^,]+, [A-Z]{2} \d{5}\z/,
+        message: "Must be in the format 123 Fake Street, New York, NY 10118" }
+
+
+
+
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   has_many :reviews, dependent: :destroy
 
+  validates :first_name, :last_name, presence: true
 end
